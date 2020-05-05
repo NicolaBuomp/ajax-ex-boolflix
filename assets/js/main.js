fc4ee970d9dbb875b5ff426286558029d025a066
Nicola <niconix.dev@gmail.com>
2020-05-05 12:54:02 +0200

    Milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,46 +9,162 @@ $(document).ready(function () {
     var template = Handlebars.compile(source);
 
 
-    button.click(function () { 
+    button.click(function () {
 
-        listMovie.html('');
-        
-        $.ajax({
-            url: 'https://api.themoviedb.org/3/search/movie',
-            method: 'GET',
-            data: {
-                api_key: '52e0283b908f5c27bb45c3eabf7e608e',
-                query: search.val().trim(),
-                language: 'it-IT',
-            },
-            success: function (res) {
-    
-                var film = res.results;
-    
-                for (var i = 0; i < film.length; i++) {
+        if (search.val() !== '') {
 
-                    search.val('');
+            $.ajax({
+                url: 'https://api.themoviedb.org/3/search/movie',
+                method: 'GET',
+                data: {
+                    api_key: '52e0283b908f5c27bb45c3eabf7e608e',
+                    query: search.val().trim(),
+                    language: 'it-IT',
+                },
+                success: function (res) {
 
-                    var thisFilm = film[i];
-    
-                    var context={
-                        title: 'Titolo: ' + thisFilm.title,
-                        originalTitle:'Titolo originale: ' + thisFilm.original_title,
-                        originalLanguage:'Lingua originale: '+ thisFilm.original_language,
-                        vote: 'Voto: ' + thisFilm.vote_average,
-                    };
-    
-                    var set = template(context);
-    
-                    listMovie.append(set);
-    
+                    var movies = res.results;
+
+                    if (movies.length > 0) {
+
+                        printMovie(template, movies, listMovie)
+                    } else {
+
+                        alert('Nessun film trovato')
+                        search.select();
+                    }
+
+                },
+                error: function () {
+                    console.log('Errore nella chiamata dei Film');
                 }
-            },
-            error: function () {
-                console.log('Errore nella chiamata dei Film');
-            }
-        });
-        
+            });
+        } else {
+            alert('ATTENZIONE, il campo è vuoto!')
+        };
+
+        if (search.val() !== '') {
+
+            $.ajax({
+                url: 'https://api.themoviedb.org/3/search/tv',
+                method: 'GET',
+                data: {
+                    api_key: '52e0283b908f5c27bb45c3eabf7e608e',
+                    query: search.val().trim(),
+                    language: 'it-IT',
+                },
+                success: function (res) {
+
+                    var Tv = res.results;
+
+                    if (Tv.length > 0) {
+
+                        printTv(template, Tv, listMovie)
+                    } else {
+
+                        alert('Nessun film trovato')
+                        search.select();
+                    }
+
+                },
+                error: function () {
+                    console.log('Errore nella chiamata dei Film');
+                }
+            });
+        } else {
+            alert('ATTENZIONE, il campo è vuoto!')
+        };
+
+        reset(listMovie);
     });
 
 });
+
+// FUNCTION
+
+// Funzione per printare i film
+function printMovie(template, movies, container) {
+
+    // reset(container);
+
+    for (var i = 0; i < movies.length; i++) {
+
+        var thisFilm = movies[i];
+
+        var context = {
+            title: thisFilm.title,
+            originalTitle: thisFilm.original_title,
+            originalLanguage: flag(thisFilm.original_language),
+            vote: star(thisFilm.vote_average),
+            tipo: 'Film'
+        };
+        var set = template(context);
+
+        container.append(set);
+    };
+};
+
+// Funzione per printare le serie tv
+function printTv(template, movies, container) {
+
+    // reset(container);
+
+    for (var i = 0; i < movies.length; i++) {
+
+        var thisTv = movies[i];
+
+        var context = {
+            title: thisTv.name,
+            originalTitle: thisTv.original_name,
+            originalLanguage: flag(thisTv.original_language),
+            vote: star(thisTv.vote_average),
+            tipo: 'Serie TV'
+        };
+
+        var set = template(context);
+
+        container.append(set);
+    };
+};
+
+// funzione di reset
+function reset(element) {
+    element.html('');
+};
+
+function star (n){
+
+    var thisNumber = Math.ceil(n / 2);
+    
+    var resultStar = '';
+
+    for (i = 0; i < thisNumber; i++){
+
+        resultStar += '<i class="fas fa-star"></i>'
+    }
+
+    if (thisNumber !== 0){
+
+        return resultStar;
+    } else {
+
+        return 'NESSUN VOTO'
+    }
+
+};
+
+function flag (flags){
+    
+    if (flags === 'en'){
+
+        return '<img class="img-flag" src="assets/img/en.svg" alt="Bandiera inglese">'
+    }else if (flags === 'it'){
+
+        return '<img class="img-flag" src="assets/img/it.svg" alt="Bandiera italiana">'
+    } else {
+
+        return flags;
+    }
+};
+
+
